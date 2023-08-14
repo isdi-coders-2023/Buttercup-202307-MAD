244f6cedec80d2b90747078698fb7ddbafb04823
Celia Avilés <celiavicaviles@gmail.com>
2023-08-14 20:49:31 +0200

    Add model file

---
+++ src/model/card.ts
@@ -0,0 +1,28 @@
+export type Card = {
+  pack_code: string;
+  pack_name: string;
+  type_code: string;
+  type_name: string;
+  sphere_code: string;
+  sphere_name: string;
+  position: number;
+  code: string;
+  name: string;
+  traits: string;
+  text: string;
+  flavor: string;
+  is_unique: boolean;
+  quantity: number;
+  deck_limit: number;
+  illustrator: string;
+  octgnid: string;
+  has_errata: boolean;
+  url: string;
+  imagesrc: string;
+  cost?: string;
+  willpower?: number;
+  attack?: number;
+  defense?: number;
+  health?: number;
+  threat?: number;
+};
