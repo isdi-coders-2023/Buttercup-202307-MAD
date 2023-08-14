44f985b1344727246600c49e9fe730f2438496e2
Rodrigo Martín <rodri.martinceron@gmail.com>
2023-08-14 18:56:38 +0200

    Add home component, style and test

---
+++ src/components/home/home.tsx
@@ -0,0 +1,31 @@
+//import style from './home.module.scss';
+export function Home() {
+  return (
+    <>
+      <main>
+        <p>
+          In The Lord of the Rings: The Card Game, players assemble a band of
+          adventurers who are trying to complete dangerous quests in
+          Middle-earth. From the brilliant fields of the Shire and the
+          treacherous paths of the Mirkwood Forest to the mighty realms of
+          Gondor and Rohan, the heroes of this beloved land unite to resist the
+          threat of the Dark Lord, Sauron.
+        </p>
+        <article>
+          <h2>The Lord of the Rings: The Card Game</h2>
+          <img src="../../assets/caja-base.jpg" alt="Caja de inicio" />
+          <div>
+            <img
+              src="../../assets/cartas-caja-base.jpg"
+              alt="cartas de caja de inicio"
+            />
+            <img
+              src="../../assets/detalles-caja-base.jpg"
+              alt="detalles de caja de inicio"
+            />
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
