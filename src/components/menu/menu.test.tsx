11ef60f436678523b31030b5f305494e58d2b237
martiap <martiap93@gmail.com>
2023-08-15 20:19:12 +0200

    Add menu test

--- src/components/menu/menu.test.tsx
+++ src/components/menu/menu.test.tsx
@@ -1,13 +1,25 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Menu } from './menu';
 
-describe('Given the componente Header', () => {
+describe('Given the componente Menu', () => {
   describe('When we render it', () => {
     render(<Menu></Menu>);
     test('the component should be in the document', () => {
       const element = screen.getByText('Create');
       expect(element).toBeInTheDocument();
+    });
+  });
+  describe('Menu', () => {
+    test('should toggle menu visibility when menu button is clicked', () => {
+      const { getByAltText, getByText } = render(<Menu />);
+      const Button = getByAltText('');
+      const home = getByText('Home');
+      expect(home).toBeVisible();
+      fireEvent.click(Button);
+      expect(home).toBeVisible();
+      fireEvent.click(Button);
+      expect(home).toBeVisible();
     });
   });
 });
