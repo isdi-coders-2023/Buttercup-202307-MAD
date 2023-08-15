759645016ff55911cc75b339741f590e36d592a1
martiap <martiap93@gmail.com>
2023-08-15 13:14:34 +0200

    Add styles to menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,29 +1,29 @@
 import styles from './menu.module.scss';
 export function Menu() {
+  const displayMenu = () => {};
   return (
     <>
-      <div>
+      <nav className={styles.nav}>
         <a href="#" className={styles.a}>
           <img src="./assets/menu (1).png" alt="" className={styles.img} />
         </a>
-        <div>
-          <ul>
-            <li>
+        <div onClick={displayMenu} className={styles.listContainer}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
               <a href="#">Home</a>
             </li>
-            <li>
+            <li className={styles.li}>
               <a href="#"> Cards</a>
             </li>
-            <li>
+            <li className={styles.li}>
               <a href="#">Create</a>
             </li>
-            <li>
+            <li className={styles.li}>
               <a href="#">Favourites</a>
             </li>
           </ul>
         </div>
-      </div>
-      <nav> </nav>
+      </nav>
     </>
   );
 }
