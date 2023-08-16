29b9959823b062bd3e21f5a0694f0c64299e9152
martiap <martiap93@gmail.com>
2023-08-16 13:27:56 +0200

    Make changes in menu and menu test with the last updates to pass sonar

--- src/components/menu/menu.test.tsx
+++ src/components/menu/menu.test.tsx
@@ -1,24 +1,33 @@
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { Menu } from './menu';
 
 describe('Given the componente Menu', () => {
   describe('When we render it', () => {
-    render(<Menu></Menu>);
+    render(
+      <MemoryRouter>
+        <Menu></Menu>
+      </MemoryRouter>
+    );
     test('the component should be in the document', () => {
       const element = screen.getByText('Create');
       expect(element).toBeInTheDocument();
     });
   });
   describe('Menu', () => {
-    test('should toggle menu visibility when menu button is  clicked', () => {
-      const { getByAltText, getByText } = render(<Menu />);
-      const burguer = getByAltText('');
-      const home = getByText('Home');
+    test('should toggle menu visibility when menu button is clicked', () => {
+      render(
+        <MemoryRouter>
+          <Menu></Menu>
+        </MemoryRouter>
+      );
+      const Button = screen.getByAltText('');
+      const home = screen.getByText('Home');
       expect(home).toBeVisible();
-      fireEvent.click(burguer);
+      fireEvent.click(Button);
       expect(home).toBeVisible();
-      fireEvent.click(burguer);
+      fireEvent.click(Button);
       expect(home).toBeVisible();
     });
   });
