7c13e6b33ab21f1247ebd8d8539b07b40352787f
Rodrigo Martín <rodri.martinceron@gmail.com>
2023-08-20 19:51:29 +0200

    Add cards test

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
     <main className={styles.error}>
       <span className={styles.text}>Error 404</span>
       <img src="../../assets/favicon.png" alt="ring" className={styles.img} />
-      <span className={styles.text}>Anillo not found</span>
+      <span className={styles.text}>Ring not found</span>
       <a href="/home" className={styles.a}>
         HOME
       </a>
